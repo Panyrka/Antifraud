16cc63d86cd62e137066d4197a190d896e261789
sawate <sawate@deeplay.io>
2023-04-07 21:05:31 +0600

    [*] Изменение основной части

--- AntiFraudServer/main.py
+++ AntiFraudServer/main.py
@@ -1,59 +1,53 @@
-from flask import Flask,request,render_template,redirect, Response
-from werkzeug.utils import secure_filename
+from flask import Flask, request, Response
 from dict2obj import Dict2Obj
-from rules import *
 
-import os
-import glob
-import json
+import AntiFraudLogic
 import logging
+import database
+import config
+import PlatFormLists
+import AntiFraudLogic
+import AFRules
 
-app = Flask(__name__)
+def get_user_config():
+    return config.UserConfig
 
-channelList = ['Web']
+def get_app_config():
+    return config.ApplicationConfig
 
-def checkChannel(transaction):
-    print('Check Transaction Started')
-    if transaction.channel in channelList:
-        print('Check Transaction Successfully Finished')
-        return True, ''
-    else:
-        print('Check Transaction Error: Transaction Channel')
-        return False, 'Bad transaction channel!'
+def db_connect(user_config):
+    db = database.DatabaseHandler(user_config)
+    db.create_connection()
+    return db
 
-def checkRules(transaction):
-    print('Check Rules Started')
-    results = []
-    for rule in rules:
-        result = rule(transaction)
-        results.append(result)
-    print('Check Rules Finished')
-    return results
+def getAFLogic():
+    user_config = get_user_config()
+    app_config = get_app_config()
+    db = db_connect(user_config)
+    pl = PlatFormLists.ListsHandler(db, app_config)
+    pl.update_list_of_lists_and_content_from_db()
+    rules = AFRules.rules
+    channelList = ['Web']
+    afLogic = AntiFraudLogic.AFLogic()
+    afLogic.initializeContext(pl, rules, channelList)
+    return afLogic
 
-def saveRulesResults(transaction, results):
-    print('For client ', transaction.clientName, ':')
-    for r in results:
-        print(r)
-
-def transactionHandler(transaction):
-    successfully, desc = checkChannel(transaction)
-    if not successfully:
-        return Response(response=desc, status=503)
+def main():
+    afLogic = getAFLogic()
     
-    rulesResult = checkRules(transaction)
-    saveRulesResults(transaction, rulesResult)
+    app = Flask(__name__)
+    @app.route('/transactions',methods = ["POST"])
+    def parse_request():
+        try:
+            data = request.json
+            transaction = Dict2Obj(data)
+            response = afLogic.transactionHandler(transaction)
+            return response
+        except Exception as e:
+            logging.error(e)
+            return Response(response = "Bad message!", status = 503)
 
-    return Response(response = 'OK', status = 200)
+    app.run(debug=True,port=2000)
 
-@app.route('/transactions',methods = ["POST"])
-def parse_request():
-    try:
-        data = request.json
-        transaction = Dict2Obj(data)
-        response = transactionHandler(transaction)
-        return response
-    except Exception as e:
-        logging.error(e)
-        return Response(response = "Bad message!", status = 503)
-
-app.run(debug=True,port=2000)
+if __name__ == '__main__':
+    main()
--- TransactionSender/main.py
+++ TransactionSender/main.py
@@ -13,7 +13,7 @@ data = {
     "cardKey": "2200567812345678",
     "phone": "+73332221100",
     "clientName": "Igor YEYEE",
-    "payeeAccNumber": 666888,
+    "payeeAccNumber": '666888',
     "payeeName": "chel kakoito",
     "payeePhone": "888666",
     "normalizedAmount": 500,
@@ -22,9 +22,9 @@ data = {
     "normalizedDatetime": "2019-12-11 10:58:37",
     "datetime": None,
     "timezone": "no",
-    "clientIp": "222.222",
+    "clientIp": "105.10.10.2",
     "clientLocation": "Ancap",
-    "deviceId": "Ihpone 15"
+    "deviceId": "666777"
 }
 
 json_data = json.dumps(data)
